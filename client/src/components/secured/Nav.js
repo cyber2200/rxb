3ab1b3ee4ea27c85202e29e7226bf999bbba3bb1
EC2 Default User <ec2-user@ip-172-30-0-176.ec2.internal>
2018-10-18 08:59:15 +0000

    Sytle fix

--- client/src/components/secured/Nav.js
+++ client/src/components/secured/Nav.js
@@ -5,26 +5,17 @@ import {t} from '../../trans/t'
 const Nav = () => {
     return(
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link className="navbar-brand" to="/secured">Boilerplate</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"/>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link className="nav-link" to="/secured">{t("home", "en")}</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link className="nav-link" to="/secured/page2secured">{t("page2secured", "en")}</Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link className="nav-link" to="/secured/logout">{t("logout", "en")}</Link>
-                        </li>
-                    </ul>
+            <div className="desktop-menu-div">
+                <div className="desktop-menu-item">
+                    <Link className="link-style" to="/secured">{t("home", "en")}</Link>
                 </div>
-            </nav>
+                <div className="desktop-menu-item">
+                    <Link className="link-style" to="/secured/page2secured">{t("page2secured", "en")}</Link>
+                </div>
+                <div className="desktop-menu-item">
+                    <Link className="link-style" to="/secured/logout">{t("logout", "en")}</Link>
+                </div>
+            </div>
         </div>
     )
 }
